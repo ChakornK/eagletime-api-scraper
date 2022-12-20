87a20246b021bf030b2c4a42d99b293bb6973045
ChakornK <120770627+ChakornK@users.noreply.github.com>
2022-12-20 15:51:11 -0800

    added block rotation and calendar

--- scraper.js
+++ scraper.js
@@ -8,32 +8,40 @@ var classes_api = "";
 
 async function getCalendar() {
 	console.log("fetching https://eagletime.appazur.com/a?age=1&ua=1&v=2");
-	calendar_api = await (
-		await fetch("https://eagletime.appazur.com/a?age=1&ua=1&v=2")
-	).json();
+	calendar_api = await (await fetch("https://eagletime.appazur.com/a?age=1&ua=1&v=2")).json();
 }
 getCalendar();
 
 async function getMessages() {
 	console.log("fetching https://eagletime.appazur.com/m/json?limit=50");
-	messages_api = await (
-		await fetch("https://eagletime.appazur.com/m/json?limit=50")
-	).json();
+	messages_api = await (await fetch("https://eagletime.appazur.com/m/json?limit=50")).json();
 }
 getMessages();
 
 async function getClasses() {
 	console.log("fetching https://eagletime.appazur.com/api/f");
-	classes_api = await (
-		await fetch("https://eagletime.appazur.com/api/f")
-	).json();
+	classes_api = await (await fetch("https://eagletime.appazur.com/api/f")).json();
 }
 getClasses();
+
+async function getBlock() {
+	console.log("fetching https://eagletime.appazur.com/api/a/block/today");
+	block_api = await (await fetch("https://eagletime.appazur.com/api/a/block/today")).json();
+}
+getBlock();
+
+async function getWeather() {
+	console.log("fetching https://eagletime.appazur.com/api/weather/current");
+	weather_api = await (await fetch("https://eagletime.appazur.com/api/weather/current")).json();
+}
+getWeather();
 
 setInterval(() => {
 	getCalendar();
 	getMessages();
 	getClasses();
+	getBlock();
+	getWeather();
 }, 600000); //every 10 minutes
 
 app.use(function (req, res, next) {
@@ -41,10 +49,7 @@ app.use(function (req, res, next) {
 	res.header("Access-Control-Allow-Credentials", "true");
 	res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
 	res.header("Access-Control-Expose-Headers", "Content-Length");
-	res.header(
-		"Access-Control-Allow-Headers",
-		"Accept, Authorization, Content-Type, X-Requested-With, Range"
-	);
+	res.header("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type, X-Requested-With, Range");
 	if (req.method === "OPTIONS") {
 		return res.sendStatus(200);
 	} else {
@@ -64,4 +69,10 @@ app.get("/messages", (req, res) => {
 });
 app.get("/classes", (req, res) => {
 	res.send(classes_api);
+});
+app.get("/block", (req, res) => {
+	res.send(block_api);
+});
+app.get("/weather", (req, res) => {
+	res.send(weather_api);
 });
